b90d38f48cd1d346d94149fb193954dccd74ba2a
wei-hsiang-h <hignativs154@gmail.com>
2026-08-19 23:24:48 +0800

    Add files via upload

---
+++ 2048-ai/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# 1. 安裝 Python 套件
+pip install -r requirements.txt
+
+# 2. 如果沒有 ai_core 資料夾，就從 GitHub 下載並重新命名
+if [ ! -d "ai_core" ]; then
+  git clone https://github.com/nneonneo/2048-ai.git ai_core
+fi
+
+# 3. 進入資料夾並編譯 C++ 程式碼 (產出 2048.so)
+cd ai_core
+make
+cd ..
